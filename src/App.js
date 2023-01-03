56583320ed1a3b4f66f86eac7414631ba7a4596e
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-01-03 14:58:35 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,16 @@ const Home = () => {
         <div className="page-title">
           <h1>Andréa Masselot</h1>
         </div>
+        <div className="presentation">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
+            quidem rerum odio! Paragrapher de présentation. Lorem ipsum, dolor
+            sit amet consectetur adipisicing elit. Quis, itaque, doloribus,
+            distinctio tempora a iusto id porro nihil quo ullam explicabo
+            sapiente voluptatum officiis deserunt! Rem fugit unde ea facilis?
+          </p>
+        </div>
+
         <div className="container"></div>
       </section>
       <section className="my-projects">
