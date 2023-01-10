0f56cdc32af5a3416084ed4d5a617a5ec09069a4
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-01-10 14:31:23 +0100

    projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,36 +28,39 @@ const Home = () => {
       </section>
       <section className="my-projects">
         <h1>My Projects</h1>
-        <CardProject
-          image={netflix}
-          title="Netflix"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
-        <CardProject
-          image={remix}
-          title="Remix"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
-        <CardProject
-          image={tripAdvisor}
-          title="Trip Advisor"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
-        <CardProject
-          image={marvel}
-          title="Marvel"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
-        <CardProject
-          image={vinted}
-          title="Vinted"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
-        <CardProject
-          image={happyCow}
-          title="Happy Cow"
-          description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
-        />
+        <div className="projects">
+          {" "}
+          <CardProject
+            image={netflix}
+            title="Netflix"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+          <CardProject
+            image={remix}
+            title="Remix"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+          <CardProject
+            image={tripAdvisor}
+            title="Trip Advisor"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+          <CardProject
+            image={marvel}
+            title="Marvel"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+          <CardProject
+            image={vinted}
+            title="Vinted"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+          <CardProject
+            image={happyCow}
+            title="Happy Cow"
+            description="DESCRIPTION DESCRIOPRFEHJEGHKRGJREJ"
+          />
+        </div>
       </section>
     </>
   );
