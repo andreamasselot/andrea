5513e38841eaba635336c42a4cddeb081ed9ec26
Andréa Masselot <andrea.nicolas128@gmail.com>
2023-01-10 14:44:40 +0100

    logo skills component

---
+++ src/components/Logos.js
@@ -0,0 +1,21 @@
+import react from "../assets/img/icontech/react.svg";
+import node from "../assets/img/icontech/node.svg";
+import vsCode from "../assets/img/icontech/vsCode.svg";
+import mui from "../assets/img/icontech/mui.svg";
+import androidStudio from "../assets/img/icontech/androidStudio.svg";
+import express from "../assets/img/icontech/express.svg";
+import css from "../assets/img/icontech/css.svg";
+import gitHub from "../assets/img/icontech/gitHub.svg";
+import js from "../assets/img/icontech/js.svg";
+
+const Logos = () => {
+  return (
+    <>
+      <section>
+        <p>COUCOU</p>
+      </section>
+    </>
+  );
+};
+
+export default Logos;
